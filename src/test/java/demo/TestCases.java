429160d88ef0167dc5331fae93e48752b338f37f
Mohit kumar <mohitkumar210791@gmail.com>
2024-09-22 15:04:40 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,16 +1,22 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -19,7 +25,7 @@ import java.util.logging.Level;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
-    ChromeDriver driver;
+    static ChromeDriver driver;
 
     /*
      * TODO: Write your tests here with testng @Test annotation. 
@@ -53,11 +59,139 @@ public class TestCases {
         driver.manage().window().maximize();
     }
 
+@Test
+public static void testCase01() throws InterruptedException{
+    int count=0;
+driver.get("https://www.flipkart.com/");
+WebElement Searchbox=driver.findElement(By.xpath("//input[@type='text']"));
+Searchbox.sendKeys("Washing Machine");
+Searchbox.sendKeys(Keys.ENTER);
+Thread.sleep(4000);
+List<WebElement> wb1=driver.findElements(By.xpath("//div[@class='XQDdHH']"));
+for(WebElement Wb:wb1){
+    String starvalue=Wb.getText();
+    float starval=Float.parseFloat(starvalue);
+    if(starval<=4){
+        count++;
+    }
+
+}
+System.out.println("washing machine with less than 4.2 star "+count);
+}
+
+@Test
+public static void testCase02() throws InterruptedException{
+
+
+driver.navigate().back();
+WebDriverWait mywait=new WebDriverWait(driver,Duration.ofSeconds(5));
+       boolean registerButton =mywait.until(ExpectedConditions.urlToBe("https://www.flipkart.com/"));
+Thread.sleep(2000);
+    WebElement Searchbox2=driver.findElement(By.xpath("//input[@type='text']"));
+    Searchbox2.clear();
+    Searchbox2.sendKeys("iPhone");
+    Searchbox2.sendKeys(Keys.ENTER);
+    Thread.sleep(4000);
+    
+    String path="//div[@class='UkUFwK']/span";
+      List<WebElement> ls1=driver.findElements(By.xpath(path));
+      for(WebElement wb1:ls1){
+        String dis=wb1.getText();
+        int num=dis.indexOf("%");
+        String discountvalue=dis.substring(0, num).trim();
+        int didcnum=Integer.parseInt(discountvalue);
+        if(didcnum>17){
+           String final_path=String.format("//span[normalize-space()='%s']/parent::div/parent::div/parent::div/parent::div/preceding-sibling::div/div", dis);
+           Thread.sleep(1000);
+           WebElement ws1=driver.findElement(By.xpath(final_path));
+           String titletxt=ws1.getText();
+           System.out.println(titletxt+""+dis);
+        }
+
+
+
+      }
+}
+
+@Test
+public static void testCase03() throws InterruptedException{
+    driver.navigate().back();
+    Thread.sleep(2000);
+    WebDriverWait mywait=new WebDriverWait(driver,Duration.ofSeconds(5));
+       boolean registerButton =mywait.until(ExpectedConditions.urlToBe("https://www.flipkart.com/"));
+    WebElement Searchbox2=driver.findElement(By.xpath("//input[@type='text']"));
+    Searchbox2.clear();
+    Searchbox2.sendKeys("Coffee Mug");
+    Searchbox2.sendKeys(Keys.ENTER);
+    Thread.sleep(3000);
+    driver.findElement(By.xpath("//div [contains(normalize-space(),'4') and @class='ewzVkT _3DvUAf']")).click();
+    Thread.sleep(3000);
+    List<WebElement>ls3=driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+    ArrayList<Integer>al1=new ArrayList<>();
+    for(WebElement wb1:ls3){
+        String rev=wb1.getText();
+      
+      if(rev.contains(",")){
+              rev=rev.replace(",", "");
+      }
+      if(rev.contains("(")){
+        rev=rev.replace("(", "");
+}
+if(rev.contains(")")){
+    rev=rev.replace(")", "");
+}
+    int curr_num= Integer.parseInt(rev);
+
+
+al1.add(curr_num) ;}
+
+ArrayList<String>al2=new ArrayList<>();
+
+Collections.sort(al1);
+int count=0;
+for(int i=al1.size()-1;i>=0;i--){
+  int temp_int=al1.get(i);
+  String temp_str=String.valueOf(temp_int);
+  
+  String final_val=temp_str;
+  StringBuilder sb1 = new StringBuilder(final_val);
+  sb1.insert(1, ",");
+  al2.add(sb1.toString());
+  sb1.setLength(0);
+    count++;
+    if(count==5){
+        break;
+    }
+
+
+
+}
+for (String s1:al2){
+    String xpath_value=String.format("//span[contains(text() ,'%s')]/parent::div/preceding-sibling::a[@class='wjcEIp']", s1);
+    WebElement wb1=driver.findElement(By.xpath(xpath_value));
+    String title=wb1.getText();
+    String image_link=wb1.getAttribute("href");
+    System.out.println("Title value"+title+" image url"+image_link +"Rating value:"+s1);
+}
+
+
+
+
+
+        
+        
+        
+
+
+    
+  
+}
+
     @AfterTest
     public void endTest()
     {
-        driver.close();
-        driver.quit();
+       // driver.close();
+       // driver.quit();
 
     }
 }
